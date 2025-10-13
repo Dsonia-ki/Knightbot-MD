020e64b55bb74949bd27bceccc7c0831af08b8fd
Dsonia-ki <dhanny567fx@gmail.com>
2025-10-13 02:21:57 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
-const settings = {
+hiconst settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "sonny v2.2.1",
+  botOwner: 'Danz', // Your name
+  ownerNumber: '2348130222153', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
